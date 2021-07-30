e06f14533aa387573eee6acd4e091d71767c7620
Jill04 <69854931+Jill04@users.noreply.github.com>
2021-07-30 12:09:21 +0530

    Update ProxyTest.js

--- tests/ProxyTest.js
+++ tests/ProxyTest.js
@@ -25,7 +25,7 @@ contract('EternalStorageProxy', ([_, proxyOwner, tokenOwner, anotherAccount]) =>
      proxynft = await EternalStorageProxyNft.new({ from: proxyOwner })
      proxytoken = await EternalStorageProxyJungle.new({ from : proxyOwner})
      v0_jungle = await Jungle.new();
-     v0_nft = await NFTcards.new(v0_jungle.address,1627049295);
+     v0_nft = await NFTcards.new();
      impl_v0_nft = await NFTcards.at(proxynft.address);
      impl_v0_jungle = await Jungle.at(proxytoken.address); 
      accounts = await web3.eth.getAccounts();
@@ -41,17 +41,27 @@ contract('EternalStorageProxy', ([_, proxyOwner, tokenOwner, anotherAccount]) =>
           await proxytoken.upgradeTo('0', v0_jungle.address,{ from })
           await proxynft.upgradeTo('0',v0_nft.address,{ from })
           
-          await impl_v0_jungle.initialize(proxyOwner,{from});
-          var actual = await proxynft.version();
-          var expected = 0
-          assert.equal(actual, expected);
+         await impl_v0_jungle.initialize(proxyOwner,{from});
+         await impl_v0_nft.initialize(proxyOwner,impl_v0_jungle.address,1627646476,{from});
+         // await v0_nft.setAddr(v0_jungle.address,1627597713,proxyOwner,{from });
+          //var actual = await proxynft.version();
+          //var expected = 0
+          // assert.equal(actual, expected);
 
-          var actual = await proxynft.implementation();
-          var expected = v0_nft.address;
-          assert.equal(actual, expected);
+          // var actual = await proxynft.implementation();
+          // var expected = v0_nft.address;
+          // assert.equal(actual, expected);
 
          
           await impl_v0_jungle.setNftCardAddress(impl_v0_nft.address,{from});
+          var price = await impl_v0_jungle.accumulationRate();
+          console.log(price.toString());
+         // await impl_v0_nft.setAddr(impl_v0_jungle.address,1627646476,{from})
+         var p= await impl_v0_nft.NFTPrice()
+
+         console.log(p.toString());
+
+         await impl_v0_nft.mintNFT(1,{value:web3.utils.toWei('0.08','ether')});
         })
       })
     }) 
